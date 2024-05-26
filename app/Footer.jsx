389edec4334f814c31d60fa5ff12273785199d29
Cale <cale.tian@icloud.com>
2024-05-26 15:44:15 -0700

    fix ui

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 export default function Footer () {
     return (
-        <footer className="relative md:static bg-accent-4 text-off-white flex flex-col md:flex-row gap-4 justify-between items-start px-20 py-10 text-cen md:px-20">
+        <footer className="relative md:static bg-accent-4 text-off-white flex flex-col md:flex-row gap-4 justify-between items-start px-10 py-10 text-cen md:px-20">
           <div>
             <h2 className="font-bold text-2xl mb-2 font-dela">Johnny Walker's Fitness</h2>
             <h2 className='font-semibold text-lg py-5 '>Location</h2>
@@ -15,11 +15,11 @@ export default function Footer () {
             </div>
           </div>
           <div className="flex-row">
-            <div className="w-1/3 md:w-1/4 flex gap-4 md:gap-8 items-bottom">
+            <div className="flex gap-4 md:gap-8 justify-center px-16 items-center">
               <Image src="/img/AP_Black.png" width={240} height={240} alt="AP" className="invert object-contain p-2" />
               <Image src="/nutrition.png" width={240} height={240} alt="Nutrition" className="object-contain m-2" />
             </div>
-              <p className="text-xs">
+              <p className="text-xs text-center md:text-start">
                 Website by Yuxing Tian Media
               </p>
 
