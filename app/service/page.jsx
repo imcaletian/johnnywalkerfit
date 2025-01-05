587b7d8b533fc8c2b5ef953e9ad3888990934be2
Cale <cale.tian@icloud.com>
2025-01-04 18:07:11 -0800

    Revert "formatted tables"
    This reverts commit 014a1c18934b6575dfb11c424bb16561ff94dbe0.

--- app/service/page.jsx
+++ app/service/page.jsx
@@ -17,13 +17,13 @@ export default function Service() {
       <div className="w-full">
         <div className="overflow-hidden h-[4rem] md:h-[6rem] lg:h-[6rem] bg-accent-1 grid"></div>
       </div>
-      <div className=" text-white bg-local bg-center bg-cover bg-black bg-[url('https://res.cloudinary.com/dtsllu9jj/image/upload/v1696740155/johnny/IMG_6829_wjceuv.jpg')] " id="pt">
+      <div className=" text-white bg-local bg-center bg-cover bg-black bg-[url('https://res.cloudinary.com/dtsllu9jj/image/upload/v1696740155/johnny/IMG_6829_wjceuv.jpg')] ">
         <div className="p-20 backdrop-brightness-50 backdrop-blur-sm grid grid-flow-row md:grid-flow-col gap-8">
           <div className="grid gap-4 md:w-[75%]">
             <h1 className=" font-dela text-3xl">
               Individualized Personal Training
             </h1>
-            <p className=" text-sm w-full">
+            <p className=" text-sm ">
               Whether you are training to look good on the beach, to feel
               healthier (and/or stronger), to enhance a specific body part or to
               make yourself more resilient to what life throws your way
@@ -35,7 +35,7 @@ export default function Service() {
               request to enhance progress towards your goals.{" "}
             </p>
           </div>
-          <div className="md:w-full">
+          <div className="py-5">
             <Table removeWrapper hideHeader aria-label="Personal training">
               <TableHeader>
                 <TableColumn>1</TableColumn>
@@ -63,11 +63,11 @@ export default function Service() {
           </div>
         </div>
       </div>
-      <div id="nutrition" className="text-white bg-local bg-center bg-cover bg-black bg-[url('https://res.cloudinary.com/dtsllu9jj/image/upload/v1730772559/How-Nutrition-Coaching-Can-Aid-in-Mental-Clarity-and-Focus-_yxkizv.jpg')]">
+      <div className="text-white bg-local bg-center bg-cover bg-black bg-[url('https://res.cloudinary.com/dtsllu9jj/image/upload/v1730772559/How-Nutrition-Coaching-Can-Aid-in-Mental-Clarity-and-Focus-_yxkizv.jpg')]">
         <div className="p-20 backdrop-brightness-50 backdrop-blur-sm grid grid-flow-row md:grid-flow-col gap-8">
           <div className="grid gap-4 md:w-[75%]">
             <h1 className=" font-dela text-3xl">Nutrition Coaching</h1>
-            <p className=" text-sm w-full">
+            <p className=" text-sm ">
               Whether they be fat loss, muscle gain, healthier living,
               performance improvements, lifestyles changes, accountability, or
               habit change every single one of your fitness & nutrition goals
@@ -79,24 +79,28 @@ export default function Service() {
               your goals. 
             </p>
           </div>
-          <div className="md:w-full">
-            <Table removeWrapper hideHeader aria-label="Nutrition Coaching">
+          <div className="py-5">
+            <Table removeWrapper hideHeader aria-label="Personal training">
               <TableHeader>
                 <TableColumn>1</TableColumn>
                 <TableColumn>2</TableColumn>
               </TableHeader>
               <TableBody>
                 <TableRow key="1">
-                  <TableCell>3 Months</TableCell>
-                  <TableCell>$250 per Month</TableCell>
+                  <TableCell>1 on 1 Session</TableCell>
+                  <TableCell>$95</TableCell>
                 </TableRow>
                 <TableRow key="2">
-                  <TableCell>6 Months</TableCell>
-                  <TableCell>$225 per Month</TableCell>
+                  <TableCell>Partner Training (2 people)</TableCell>
+                  <TableCell>$120</TableCell>
                 </TableRow>
                 <TableRow key="3">
-                  <TableCell>12 Month</TableCell>
-                  <TableCell>$200 per Month</TableCell>
+                  <TableCell>Small Group Training (3 People)</TableCell>
+                  <TableCell>$150</TableCell>
+                </TableRow>
+                <TableRow key="4">
+                  <TableCell>Larger Group</TableCell>
+                  <TableCell>Please Consult For Rate</TableCell>
                 </TableRow>
               </TableBody>
             </Table>
@@ -105,12 +109,12 @@ export default function Service() {
         </div>
       </div>
       <div className="text-white bg-local bg-center bg-cover bg-black bg-[url('https://res.cloudinary.com/dtsllu9jj/image/upload/v1730772651/shutterstock_1700797540_nsgivu.png')]">
-        <div className="p-20 backdrop-brightness-50 backdrop-blur-sm grid grid-flow-row md:grid-flow-col gap-8">
-          <div className="grid gap-4 md:w-[75%]">
+        <div className="p-20 backdrop-brightness-50 backdrop-blur-sm grid grid-flow-row md:grid-flow-col">
+          <div className="grid gap-4 md:w-3/4">
             <h1 className=" font-dela text-3xl">
               Individualized Online Training
             </h1>
-            <p className="text-sm w-full">
+            <p className=" text-sm ">
               If you are looking for help with a specific goal but can’t do
               in-person training, online training with Johnny is an accessible
               and affordable option you can do from home or on the go. Each
@@ -119,28 +123,28 @@ export default function Service() {
               goals.{" "}
             </p>
           </div>
-          <div className="md:w-full">
-            <Table removeWrapper hideHeader aria-label="Online training">
+          <div className="py-5">
+            <Table removeWrapper hideHeader aria-label="Personal training">
               <TableHeader>
                 <TableColumn>1</TableColumn>
                 <TableColumn>2</TableColumn>
               </TableHeader>
               <TableBody>
                 <TableRow key="1">
-                  <TableCell>1 Month</TableCell>
-                  <TableCell>$299</TableCell>
+                  <TableCell>1 on 1 Session</TableCell>
+                  <TableCell>$95</TableCell>
                 </TableRow>
                 <TableRow key="2">
-                  <TableCell>3 Months</TableCell>
-                  <TableCell>$825 ($275 per month)</TableCell>
+                  <TableCell>Partner Training (2 people)</TableCell>
+                  <TableCell>$120</TableCell>
                 </TableRow>
                 <TableRow key="3">
-                  <TableCell>6 Months</TableCell>
-                  <TableCell>$1500 ($250 per month)</TableCell>
+                  <TableCell>Small Group Training (3 People)</TableCell>
+                  <TableCell>$150</TableCell>
                 </TableRow>
                 <TableRow key="4">
-                  <TableCell>12 Months</TableCell>
-                  <TableCell>$2400 ($200 per month)</TableCell>
+                  <TableCell>Larger Group</TableCell>
+                  <TableCell>Please Consult For Rate</TableCell>
                 </TableRow>
               </TableBody>
             </Table>
